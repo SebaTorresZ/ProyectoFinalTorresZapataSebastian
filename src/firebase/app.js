a65e1de12cc0f96b1b589db4744837b0d6642700
Sebastián Torres <seba.torresz@duocuc.cl>
2024-01-31 09:52:30 -0300

    modificacion variables .env

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,22 +1,14 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-
-const apiKey = import.meta.env.VITE_apiKey;
-const authDomain = import.meta.env.VITE_authDomain;
-const projectId = import.meta.env.VITE_projectId;
-const storageBucket = import.meta.env.VITE_storageBucket;
-const messagingSenderId = import.meta.env.VITE_messagingSenderId;
-const appId = import.meta.env.VITE_appId;
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey,
-  authDomain,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
+  apiKey: "AIzaSyBMPM-xMXnttB7yKXfgO1NuF-Sze6ipqIA",
+  authDomain: "joyeria-sagitario.firebaseapp.com",
+  projectId: "joyeria-sagitario",
+  storageBucket: "joyeria-sagitario.appspot.com",
+  messagingSenderId: "587094835451",
+  appId: "1:587094835451:web:f05ba696aac0a6aa5df05e",
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
